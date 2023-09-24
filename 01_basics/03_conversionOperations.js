025a7b1beaebad834677524b00c3d769c111d52c
Bisma-21 <63552650+Bisma-21@users.noreply.github.com>
2023-09-24 14:51:52 +0000

    03_conversion_of_data_types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let age= null
+ 
+ console.log(typeof age)
+const valueInNumber = Number(age)
+console.log( valueInNumber)
+console.log(typeof valueInNumber)
+
+// "33" ==> 33
+//"44gfhf" ==> NAN
+//null ==> 0
+// undefined ==> 1
+// true ==> 1
+//false ==>0
+
+let isVerified = "string"
+let booleanIsVerified = Boolean(isVerified)
+console.log(booleanIsVerified)
+
+//1 ==> true 
+//""===> false
+//"jhjj"==> true
+
+
+let data = 66
+let stringData = String(data)
+console.log(stringData)
+console.log(typeof stringData)
