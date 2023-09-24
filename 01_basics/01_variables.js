290f07868127154a4fe83253734ea2bbd5c59977
Bisma-21 <63552650+Bisma-21@users.noreply.github.com>
2023-09-24 14:13:49 +0000

    variables(let,const,var)

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const userId= 543
+let name="bisma"
+var address="eidgah"
+country ="india"
+
+name="bisma manzoor"
+address="firdous colony eidgah"
+country="India"
+console.table( [userId , name , address , country])
