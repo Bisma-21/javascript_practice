21f385f016f148f12e5f601cf00afa20c184a3c4
Bisma-21 <63552650+Bisma-21@users.noreply.github.com>
2023-09-24 14:34:01 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+let name="bisma" //string
+let age=60 //number
+let isLoggedIn // boolean
+
+/*
+ PREMETIVE DATATYPES
+number , string , boolean , bigInt
+null  => (stand alone value)  i.e representation of empty value
+undefined => ( value not defined)
+symbol => uniqueness k liyi use hota hain
+
+NON -PRE
+ object
+*/
+
+console.log(typeof(null)) // object
+
+console.log(typeof(undefined)) // undefined
